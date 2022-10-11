d42bb648d5b0cbfda220fd67270f58aeb7b5dbba
llamicron <llamicron@gmail.com>
2022-10-11 01:34:09 -0500

    Adds some comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 pub mod model;
-#[cfg(feature = "web")]
-pub mod api;
 
-use model::RTU;
+// Only compile that module if we want the web server
+// This is good for the CLI because we can use the RTU generation
+// code without compiling warp and tokio
+#[cfg(feature = "web")]
+pub mod server;
 
 #[cfg(feature = "web")]
 #[tokio::main]
 async fn main() {
     println!("About to start the web server");
-    api::run().await;
+    server::run().await;
 }
--- src/server.rs
+++ src/server.rs
@@ -3,7 +3,9 @@ use std::convert::Infallible;
 use warp::Filter;
 use crate::model::{RTU, Mode};
 
+/// Creates a warp server and runs it
 pub async fn run() {
+    // Responds to /running with a payload containing true, just for testing
     let running = warp::path("running").map(|| r#"{"running":"true"}"# );
 
     let generate_rtu_route = warp::path("generate")
@@ -24,18 +26,21 @@ pub async fn run() {
     warp::serve(routes).run(([0,0,0,0], 3012)).await;
 }
 
+/// Receives the RTU model and updates the hardware to match, aka Write mode
 async fn enact_rtu(mut rtu: RTU) -> Result<impl warp::Reply, Infallible> {
     println!("RTU recieved model, enacting changes");
     let updated = RTU::update(&mut rtu, &Mode::Write).await;
     Ok(serde_json::to_string(&updated).expect("Couldn't serialize model"))
 }
 
+/// Receives the RTU model and updates it to match the hardware, aka Read mode
 async fn update_rtu(mut rtu: RTU) -> Result<impl warp::Reply, Infallible> {
     println!("RTU recieved model, updating and sending it back");
     let updated = RTU::update(&mut rtu, &Mode::Read).await;
     Ok(serde_json::to_string(&updated).expect("Couldn't serialize model"))
 }
 
+/// Generates the RTU model from the configuration file
 async fn generate_rtu() -> Result<impl warp::Reply, Infallible> {
     println!("Got a ping, generating RTU model");
     let rtu = RTU::generate(None).expect("Couldn't generate RTU from configuration file");
